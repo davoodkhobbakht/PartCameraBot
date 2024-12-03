7ab69a9c3b7e63406b43082d29cf8ae0e628910a
davoodkhobbakht <davoodkhobbakht@gmail.com>
2024-12-03 16:22:17 +0330

    add print

--- worker.py
+++ worker.py
@@ -1273,7 +1273,7 @@ class Worker(threading.Thread):
                 self.bot.send_message(self.chat.id, f"{prompt} (پیش‌فرض: {default_value})", reply_markup=keyboard)
 
                 response = self.__wait_for_inlinekeyboard_callback()
-                
+                print(response)
                 if isinstance(response, CancelSignal):
                     return "cancelled"  # Exit process
                 elif response == "⬅️ بازگشت":

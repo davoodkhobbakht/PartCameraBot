618d166e04e441929e190a707b1cf1000e5f84f1
davoodkhobbakht <davoodkhobbakht@gmail.com>
2024-12-03 17:38:59 +0330

    fix orders menu to send orders with image

--- worker.py
+++ worker.py
@@ -1822,8 +1822,11 @@ class Worker(threading.Thread):
         # Create a message for every one of them
         for order in orders:
             # Send the created message
-            self.bot.send_message(self.chat.id, order.text(w=self),
-                                  reply_markup=order_keyboard)
+            #self.bot.send_message(self.chat.id, order.text(w=self),reply_markup=order_keyboard)
+            
+            payment_image = open(order.payment_image.file_path , 'r')
+            self.bot.send_photo(self.chat.id, payment_image, caption=self.loc.get('notification_order_placed',
+                                               order=order.text(w=self)))
         # Set the Live mode flag to True
         self.admin.live_mode = True
         # Commit the change to the database
